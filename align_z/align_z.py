60cac74edbd5bab791a71d2add8b3746289f7701
ValGillet <val.gillet12@gmail.com>
2025-09-11 15:48:34 +0200

    removed obsolete align_arrays_z + changed mesh relaxation to track vmax

--- align_z/align_z.py
+++ align_z/align_z.py
@@ -475,101 +475,117 @@ def get_inv_map(flow, stride, dataset_name, mesh_config=None):
     return inv_map, flow_bbox
 
 
-def align_arrays_z(prev, 
-                   curr, 
-                   scale, 
-                   patch_size, 
-                   stride, 
-                   max_magnitude,
-                   max_deviation,
-                   k0,
-                   k,
-                   gamma,
-                   filter_size, 
-                   range_limit,
-                   num_threads=0):
-    
-    output_shape = curr.shape
- 
-    mfc = flow_field.JAXMaskedXCorrWithStatsCalculator()
-    data = [[prev, curr], 
-            [resize(prev, None, fy=scale, fx=scale),
-             resize(curr, None, fy=scale, fx=scale)]]
 
-    flows = []
-    for i, (prev, curr) in enumerate(data):
-        prev_mask = compute_mask(prev, filter_size, range_limit)
-        curr_mask = compute_mask(curr, filter_size, range_limit)
+def relax_mesh_mod(
+    x: jnp.ndarray,
+    prev: jnp.ndarray | None,
+    config: IntegrationConfig,
+    mesh_force=inplane_force,
+    prev_fn=None,
+) -> tuple[jnp.ndarray, list[float], int]:
+  """Simulates mesh relaxation.
 
-        # Make shapes match
-        if np.any(np.array(curr.shape) > np.array(prev.shape)):
-            # If prev is smaller, we pad to shape with zeros to the end of the data
-            # It doesn't affect offset
-            prev = pad_to_shape(prev, curr.shape)
-            prev_mask = pad_to_shape(prev_mask, curr.shape)
-        if np.any(np.array(prev.shape) > np.array(curr.shape)):
-            # If prev is larger, we crop to shape
-            # Prev and curr should be roughly overlapping, so we should not be losing relevant info
-            y,x = curr.shape
-            prev = prev[:y, :x]
-            prev_mask = prev_mask[:y, :x]
-        
-        if i == 0:
-            # Store the arrays for output
-            prev_align = prev
-            curr_align = curr
-        
-        try:
-            flow = mfc.flow_field(prev, curr, (patch_size, patch_size),
-                                (stride, stride), batch_size=256,
-                                pre_mask=prev_mask, post_mask=curr_mask)
-        except Exception as e:
-            raise RuntimeError(e)
-        
-        flows.append(np.transpose(flow[None, ...], [1, 0, 2, 3])) # [channels, z, y, x]
+  Args:
+    x: [2, z, y, x] array of mesh node positions
+    prev: optional [2, z, y, x] array against which to compute the force due to
+      0-length springs
+    config: simulation parameters
+    mesh_force: callable with the signature of `inplane_force` returning a field
+      representing internal mesh forces
+    prev_fn: callable taking the 'x' mesh array and returning the 'prev' array
 
-    flow, ds_flow = flows
+  Returns:
+    tuple of:
+      [2, z, x, y] array of updated mesh positions
+      list of kinetic energy history
+      number of simulation steps executed
+  """
 
-    pad = patch_size // 2 // stride
-    flow = np.pad(flow, [[0, 0], [0, 0], [pad, pad], [pad, pad]], constant_values=np.nan)
-    ds_flow = np.pad(ds_flow, [[0, 0], [0, 0], [pad, pad], [pad, pad]], constant_values=np.nan)
+  t = 0
+  v = jnp.zeros_like(x)
+  dt = config.dt
+  alpha = config.alpha
+  e_kin = []
+  cap = config.start_cap
 
-    flow = flow_utils.clean_flow(flow, 
-                                 min_peak_ratio=1.6, 
-                                 min_peak_sharpness=1.6, 
-                                 max_magnitude=max_magnitude, 
-                                 max_deviation=max_deviation)
-    ds_flow = flow_utils.clean_flow(ds_flow, 
-                                    min_peak_ratio=1.6, 
-                                    min_peak_sharpness=1.6, 
-                                    max_magnitude=0, 
-                                    max_deviation=0)
-    
-    ds_flow_hires = np.zeros_like(flow)
+  if config.start_cap != config.final_cap:
+    if not config.fire:
+      raise NotImplementedError(
+          'Adaptive force capping is only supported with FIRE.'
+      )
+    if config.cap_scale <= 1:
+      raise ValueError(
+          'The scaling factor for the force cap has to be larger '
+          'than 1 when the initial and final cap are different.'
+      )
 
-    bbox = bounding_box.BoundingBox(start=(0, 0, 0), 
-                                    size=(flow.shape[-1], flow.shape[-2], 1))
-    bbox_ds = bounding_box.BoundingBox(start=(0, 0, 0), 
-                                       size=(ds_flow.shape[-1], ds_flow.shape[-2], 1))
+  if prev is not None and prev_fn is not None:
+    raise ValueError('Only one of: "prev" and "prev_fn" can be specified.')
+  
+  while t < config.max_iters:
+    state = velocity_verlet(
+        x,
+        v,
+        prev,
+        config,
+        fire_dt=dt,
+        fire_alpha=alpha,
+        force_cap=cap,
+        mesh_force=mesh_force,
+        prev_fn=prev_fn,
+    )
+    t += config.num_iters
+    x, v = state[:2]
+    v_mag = jnp.linalg.norm(v, axis=0)
+    e_kin.append(float(jnp.sum(v_mag**2)))
+    v_max = jnp.max(v_mag)
 
-    # Upsample and scale spatial components.
-    resampled = map_utils.resample_map(
-                    ds_flow[:, 0:1, ...], 
-                    bbox_ds, bbox, 2, 1)
-    ds_flow_hires[:, 0:1, ...] = resampled / scale
+    if config.fire:
+      dt, alpha, n_pos, cap = state[-4:]
+    #   logging.info(
+    #       't=%r: dt=%f, alpha=%f, n_pos=%d, cap=%f, v_max=%f, e_kin=%f',
+    #       t,
+    #       dt,
+    #       alpha,
+    #       n_pos,
+    #       cap,
+    #       v_max,
+    #       e_kin[-1],
+    #   )
 
-    flow = flow_utils.reconcile_flows((flow, ds_flow_hires), 
-                                       max_gradient=0, max_deviation=0, min_patch_size=400)
+    if v_max < config.stop_v_max:
+      if cap >= config.final_cap:
+        break
 
-    mesh_config = mesh.IntegrationConfig(dt=0.001, gamma=gamma, k0=k0, k=k, stride=(stride, stride), num_iters=1000,
-                                         max_iters=100000, stop_v_max=0.005, dt_max=1000, start_cap=0.01,
-                                         final_cap=10, prefer_orig_order=True)
-    inv_map, flow_bbox = get_inv_map(flow, stride, 'Test', mesh_config)
+      # Increase cap to ensure progress towards the termination condition.
+      cap = min(cap * config.cap_scale, config.final_cap)
 
-    data_bbox = bounding_box.BoundingBox(start=(0, 0, 0), 
-                                         size=(output_shape[-1], output_shape[-2], 1))
+  return x, e_kin, t, v_max # mod
 
-    aligned = warp.warp_subvolume(curr_align[None, None, ...], data_bbox, inv_map[:, 1:2, ...], 
-                                  flow_bbox, stride, data_bbox, 'lanczos', parallelism=num_threads)
-    
-    return np.stack([prev_align, curr_align]), np.stack([prev_align, aligned.squeeze()])
+
+
+def get_inv_map_mod(flow, stride, dataset_name, mesh_config=None):
+
+    if mesh_config is None:
+        mesh_config = mesh.IntegrationConfig(dt=0.001, gamma=0.5, k0=0.01, k=0.1, stride=(stride, stride), num_iters=1000,
+                                            max_iters=100000, stop_v_max=0.005, dt_max=1000, start_cap=0.01,
+                                            final_cap=10, prefer_orig_order=True)
+
+    solved = [np.zeros_like(flow[:, 0:1, ...])]
+    origin = jnp.array([0., 0.])
+    all_vmax = []
+    for z in tqdm(range(0, flow.shape[1]),
+                  desc=f'{dataset_name}: Relaxing mesh'):
+        prev = map_utils.compose_maps_fast(flow[:, z:z+1, ...], origin, stride,
+                                           solved[-1], origin, stride)
+        x, _, _, v_max = relax_mesh_mod(np.zeros_like(solved[0]), prev, mesh_config)
+        solved.append(np.array(x))
+        all_vmax.append(v_max)
+
+    solved = np.concatenate(solved, axis=1)
+
+    flow_bbox = bounding_box.BoundingBox(start=(0, 0, 0), size=(flow.shape[-1], flow.shape[-2], 1))
+
+    inv_map = map_utils.invert_map(solved, flow_bbox, flow_bbox, stride)
+
+    return inv_map, flow_bbox, np.array(all_vmax)
